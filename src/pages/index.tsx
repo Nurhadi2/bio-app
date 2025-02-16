bba6484108fbe231ee0cf5ddbc30e50d46839458
Nurhadi19 <nurhadisasono002@gmail.com>
2025-02-16 23:01:34 +0800

    refactor: menambahkan styling pada index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,69 @@
+import { Outfit } from "next/font/google";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outtfit = Outfit({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const data = [
+  {
+    id: 1,
+    title: "instagram",
+    link: "https://www.instagram.com/adi_sasono30/",
+    icon: <FaInstagram />,
+  },
+  {
+    id: 2,
+    title: "linkedin",
+    link: "https://www.linkedin.com/in/nurhadi-sasono-bb1bbb232/",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 3,
+    title: "github",
+    link: "https://www.instagram.com/adi_sasono30/",
+    icon: <FaGithub />,
+  },
+];
 
-export default function Home() {
+const Home = () => {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    <main
+      className={`${outtfit.className} bg-gradient-to-br from-[#abdcff] to-[#0396ff] text-slate-900`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <section className="min-h-screen flex flex-col items-center justify-center container mx-auto px-4 py-16 lg:max-w-md">
+        <div className="flex flex-col justify-center items-center text-center">
+          <Image
+            src="/profile.png"
+            alt="foto-profile"
+            width={120}
+            height={120}
+            className="rounded-full border-4 border-white shadow-lg"
+          />
+          <h1 className="text-3xl font-bold  mt-3 text-white">
+            Nurhadi Sasono
+          </h1>
+          <p className="w-3/4 text-white opacity-90">Fullstack Web Developer</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="mt-6 flex flex-col gap-3 w-full">
+          {data.map(
+            (data: { id: number; title: string; link: string; icon: any }) => (
+              <Link
+                key={data.id}
+                href={data.link}
+                className="flex items-center gap-3 justify-center w-full text-lg font-semibold border-2 border-white p-3 rounded-lg bg-white/20 shadow-md backdrop-blur-sm text-white hover:bg-white hover:text-black hover:-translate-y-1 transition-all duration-200"
+              >
+                {data.icon}
+                {data.title}
+              </Link>
+            )
+          )}
+        </div>
+      </section>
+    </main>
   );
-}
+};
+
+export default Home;
